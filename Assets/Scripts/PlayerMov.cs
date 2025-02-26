fbd6abe920746c77b54c0d05f9d3b5d8c094ac15
Javier495 <javierpenas025@gmail.com>
2025-02-26 17:55:31 +0100

    no fuimo

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -1,16 +1,59 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMov : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed;
+    public float jumpHeight;
+    public float gravity;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private Vector2 moveInput;
+    private bool jumpInput;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Comprobar si está en el suelo
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // Pequeño empuje hacia abajo para asegurar contacto con el suelo
+        }
+
+        // Movimiento horizontal
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Salto
+        if (jumpInput && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * 2f * gravity);
+            jumpInput = false; // Resetear el input del salto
+        }
+
+        // Aplicar gravedad
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumpInput = true;
+        }
     }
 }
+
